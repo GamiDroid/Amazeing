03ffc961869e3a51ff994c15e5a7f5eff0761f0b
Kevin Damen <k.damen@bellt.nl>
2023-12-01 22:00:36 +0100

    Init connection with amazeing

--- Amazeing/Program.cs
+++ Amazeing/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Net.Http.Headers;
+
+using var httpclient = new HttpClient
+{
+    BaseAddress = new Uri("https://maze.hightechict.nl/"),
+};
+
+httpclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", "HTI Thanks You [9aA]");
+
+var response = await httpclient.GetAsync("/api/mazes/all");
+
+response.EnsureSuccessStatusCode();
+
+var stringContent = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(stringContent);
+
+Console.ReadLine();
