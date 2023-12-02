deaf0efb9cd128bf280b0b1f5bd61d65c48a7ce9
Kevin Damen <k.damen@bellt.nl>
2023-12-02 09:25:50 +0100

    Maze repository for getting maze data
    maze repo gets maze info from the API, then stores maze infos in a local database, for later use.

--- Amazeing/Program.cs
+++ Amazeing/Program.cs
@@ -1,21 +1,32 @@
 ﻿using Amazeing;
 using Amazeing.Models;
-﻿
+using Amazeing.Repositories;
+using Spectre.Console;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
 AmazeingDbContext.CreateOrUpdateModel();
 
-using var httpclient = new HttpClient
+var httpclient = new HttpClient
 {
     BaseAddress = new Uri("https://maze.hightechict.nl/"),
 };
-
 httpclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", "HTI Thanks You [9aA]");
 
-var response = await httpclient.GetAsync("/api/mazes/all");
+using var mazeRepo = new MazeRepo(httpclient);
 
-response.EnsureSuccessStatusCode();
+var mazes = await mazeRepo.GetAllMazeInfosAsync();
 
-var stringContent = await response.Content.ReadAsStringAsync();
+var grid = new Grid();
+grid.AddColumns(3);
 
-Console.WriteLine(stringContent);
+grid.AddRow(nameof(MazeInfo.Name), nameof(MazeInfo.TotalTiles), nameof(MazeInfo.PotentialReward));
+
+foreach (var maze in mazes)
+{
+    grid.AddRow(maze.Name ?? "", maze.TotalTiles.ToString(), maze.PotentialReward.ToString());
+}
+
+AnsiConsole.Write(grid);
 
 Console.ReadLine();
---
+++ Amazeing/Repositories/MazeRepo.cs
@@ -0,0 +1,40 @@
+﻿using Amazeing.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Json;
+
+namespace Amazeing.Repositories;
+internal class MazeRepo : IDisposable
+{
+    private readonly HttpClient _httpClient;
+
+    public MazeRepo(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<ICollection<MazeInfo>> GetAllMazeInfosAsync()
+    {
+        using var db = new AmazeingDbContext();
+
+        var mazes = await db.Mazes.AsNoTracking().ToListAsync();
+
+        if (mazes.Count == 0)
+        {
+            mazes = await _httpClient.GetFromJsonAsync<List<MazeInfo>>("/api/mazes/all") ?? [];
+
+            if (mazes.Count != 0)
+            {
+                // Add maze infos to local storage
+                db.Mazes.AddRange(mazes);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        return mazes;
+    }
+
+    public void Dispose()
+    {
+        _httpClient?.Dispose();
+    }
+}
